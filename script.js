8c6b8233588b0be61216efb365c892b1bce83fb1
SHON PURDY <SHONPURDY@GMAIL.COM>
2023-06-05 11:29:09 -0700

    Add Delete Button

--- script.js
+++ script.js
@@ -1,58 +1,64 @@
 $(document).ready(function(){
-    $.ajax({
-      type: 'GET',
-      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=210',
-      dataType: 'json',
-      success: function (response, textStatus) {
-        response.tasks.forEach(function (task) {
-        $('#todo-list').append('<p>' + task.content + '</p>');
-        })
-      },
-      error: function (request, textStatus, errorMessage) {
-        console.log(errorMessage);
-      }
-    });
-
-    $.ajax({
-        type: 'POST',
-        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=2',
-        contentType: 'application/json',
+    var getAndDisplayAllTasks = function () {
+      $.ajax({
+        type: 'GET',
+        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=210',
         dataType: 'json',
-        data: JSON.stringify({
-          task: {
-            content: 'Do something fun'
-          }
-        }),
         success: function (response, textStatus) {
-          console.log(response);
+            $('#todo-list').empty();
+          response.tasks.forEach(function (task) {
+            $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button>');
+            });
         },
         error: function (request, textStatus, errorMessage) {
           console.log(errorMessage);
         }
       });
+    }
+    
+    var createTask = function () {
+      $.ajax({
+        type: 'POST',
+        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=210',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({
+          task: {
+            content: $('#new-task-content').val()
+          }
+        }),
+        success: function (response, textStatus) {
+        $('#new-task-content').val('');
+          getAndDisplayAllTasks();
+        },
+        error: function (request, textStatus, errorMessage) {
+          console.log(errorMessage);
+        }
+      });  
+    }
+    
+    $('#create-task').on('submit', function (e) {
+      e.preventDefault();
+      createTask();
+    });
+    
+    getAndDisplayAllTasks();
 
-      var createTask = function () {
+    var deleteTask = function (id) {
         $.ajax({
-          type: 'POST',
-          url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=210',
-          contentType: 'application/json',
-          dataType: 'json',
-          data: JSON.stringify({
-            task: {
-              content: $('#new-task-content').val()
-            }
-          }),
+       type: 'DELETE',
+          url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '?api_key=210',
           success: function (response, textStatus) {
-            console.log(response);
+            getAndDisplayAllTasks();
           },
           error: function (request, textStatus, errorMessage) {
             console.log(errorMessage);
           }
-        });  
+        });
       }
 
-      $('#create-task').on('submit', function (e) {
-        e.preventDefault();
-        createTask();
+      $(document).on('click', '.delete', function () {
+        deleteTask($(this).data('id'));
       });
+    
   });
