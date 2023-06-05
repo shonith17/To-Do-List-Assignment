e82bd5e14776fec45bca172d4919ff5ca50eff50
SHON PURDY <SHONPURDY@GMAIL.COM>
2023-06-05 11:13:48 -0700

    Add Form to HTML. Add 'POST' function to JS

--- script.js
+++ script.js
@@ -30,4 +30,29 @@ $(document).ready(function(){
           console.log(errorMessage);
         }
       });
+
+      var createTask = function () {
+        $.ajax({
+          type: 'POST',
+          url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=210',
+          contentType: 'application/json',
+          dataType: 'json',
+          data: JSON.stringify({
+            task: {
+              content: $('#new-task-content').val()
+            }
+          }),
+          success: function (response, textStatus) {
+            console.log(response);
+          },
+          error: function (request, textStatus, errorMessage) {
+            console.log(errorMessage);
+          }
+        });  
+      }
+
+      $('#create-task').on('submit', function (e) {
+        e.preventDefault();
+        createTask();
+      });
   });
