6508080466236e6922be9ec9594cffa9ca3dcf29
SHON PURDY <SHONPURDY@GMAIL.COM>
2023-06-05 11:33:45 -0700

    Add complete task function

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ $(document).ready(function(){
         dataType: 'json',
         success: function (response, textStatus) {
             $('#todo-list').empty();
-          response.tasks.forEach(function (task) {
-            $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button>');
-            });
+            response.tasks.forEach(function (task) {
+                $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '>');
+              });
         },
         error: function (request, textStatus, errorMessage) {
           console.log(errorMessage);
@@ -60,5 +60,25 @@ $(document).ready(function(){
       $(document).on('click', '.delete', function () {
         deleteTask($(this).data('id'));
       });
-    
+
+      var markTaskComplete = function (id) {
+        $.ajax({
+       type: 'PUT',
+          url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_complete?api_key=2',
+          dataType: 'json',
+          success: function (response, textStatus) {
+            getAndDisplayAllTasks();
+          },
+          error: function (request, textStatus, errorMessage) {
+            console.log(errorMessage);
+          }
+        });
+      }
+
+      $(document).on('change', '.mark-complete', function () {
+        if (this.checked) {
+           markTaskComplete($(this).data('id'));
+         }
+       });
+       
   });
